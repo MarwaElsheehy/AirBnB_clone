3cb9f59c508c74e6e225c399570956e85007234b
Rahma <Rahma@gmail.com>
2023-10-14 11:06:17 -0700

    a

---
+++ models/base_model.py
@@ -0,0 +1,25 @@
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        """Initialize BaseModel instance."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
+
+    def save(self):
+        """Update the public instance attribute updated_at with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary containing all keys/values of __dict__ of the instance."""
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
